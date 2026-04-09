02ddfb78975aa2a9fab8c3e7eefc328d1f335bfa
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-09 10:07:14 -0600

    change font-family

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen gap-10">
-        <h1 className="font-mono text-4xl font-bold text-zinc-500">
-          Zustand Playground
-        </h1>
+        <h1 className="text-4xl font-bold text-zinc-500">Zustand Playground</h1>
         <h2 className="text-2xl font-bold text-zinc-500">Name: {name}</h2>
         <h2 className="text-2xl font-bold text-zinc-500">Grade: {grade}</h2>
         <div>
